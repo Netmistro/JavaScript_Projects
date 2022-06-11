bb6d6b4b1006128db2a1e9fc3a99707ae4b20916
Arnold Bradshaw <arnoldbradshaw@hotmail.com>
2022-06-11 15:27:14 -0400

    Update

--- Color Flipper/app.js
+++ Color Flipper/app.js
@@ -1,20 +1,22 @@
 const colors = [
-  'Green',
-  'Red',
-  'Purple',
-  'limegreen',
-  'orange',
-  'blue',
-  'rgba(133,122,200',
-  '#f15025',
+  "Green",
+  "Red",
+  "Purple",
+  "limegreen",
+  "orange",
+  "blue",
+  "rgba(133,122,200",
+  "#f15025",
 ];
-const btn = document.getElementById('btn');
-const color = document.querySelector('.color');
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
 
-btn.addEventListener('click', function () {
+btn.addEventListener("click", function () {
   // get a random nymber between 0-3
   const randomNumber = getRandomNumber();
+  // Test using coneole log
   console.log(randomNumber);
+  console.log(Math.random());
 
   document.body.style.backgroundColor = colors[randomNumber];
   color.textContent = colors[randomNumber];
