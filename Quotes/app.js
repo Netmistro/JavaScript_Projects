ee7f544f88bd533a138388368adb8b0939cd3e20
Arnold Bradshaw <arnoldbradshaw@hotmail.com>
2022-06-13 15:58:00 -0400

    Update

---
+++ Quotes/app.js
@@ -0,0 +1,12 @@
+const quotes = [
+  { name: "Stephen King", quote: "This is the life" },
+  {
+    name: "Demi Moore",
+    quote:
+      "I'm a success today because I had a friend who believed in me and I don't have the heart to et him down",
+  },
+  {
+    name: "John F. Kennedy",
+    quote: "Those who dare to fail miserably can chieve greatly",
+  },
+];
