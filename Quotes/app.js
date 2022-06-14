cb2a3617024266dd0b768343e91479af4d3baab7
Arnold Bradshaw <arnoldbradshaw@hotmail.com>
2022-06-14 16:09:25 -0400

    Structure complete

--- Quotes/app.js
+++ Quotes/app.js
@@ -9,4 +9,25 @@ const quotes = [
     name: "John F. Kennedy",
     quote: "Those who dare to fail miserably can chieve greatly",
   },
+  {
+    name: "Ernest Hemingway",
+    quote:
+      "It’s none of their business that you have to learn how to write. Let them think you were born that way.",
+  },
 ];
+
+// Issue constants and variables here
+const quoteBtn = document.querySelector(".btn-quote");
+const quote = document.querySelector(".quote");
+const author = document.querySelector(".author");
+
+// Event Listeners here
+quoteBtn.addEventListener("click", displayQuote);
+
+// Functions here
+function displayQuote() {
+  // Generate a random number from the array list
+  randomQuote = Math.floor(Math.random() * quotes.length);
+  quote.innerHTML = quotes[randomQuote].name;
+  author.innerHTML = quotes[randomQuote].quote;
+}
