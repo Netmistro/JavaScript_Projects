c12e22aefcd694bfabf91d315a713205915753ef
Arnold Bradshaw <arnoldbradshaw@hotmail.com>
2022-06-15 23:50:26 -0400

    Complete

---
+++ Navigation_Bar/app.js
@@ -0,0 +1,15 @@
+// Define all variables here
+const navbarBtn = document.querySelector('.navbar__btn');
+const navbarLinks = document.querySelector('.navbar__links');
+
+// Add event listeners here
+navbarBtn.addEventListener('click', function () {
+  let value = navbarLinks.classList.contains('navbar__collapse');
+  if (value) {
+    navbarLinks.classList.remove('navbar__collapse');
+    navbarBtn.classList.remove('change');
+  } else {
+    navbarLinks.classList.add('navbar__collapse');
+    navbarBtn.classList.add('change');
+  }
+});
